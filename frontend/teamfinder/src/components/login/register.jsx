f716266cf5f6a7e109ecf52bb5a4cb888a202eb7
Jonas Eszter <s7p5ga@inf.elte.hu>
2021-10-04 18:54:11 +0200

    Create basic login page

---
+++ frontend/teamfinder/src/components/login/register.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+export class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="base-container">
+            <div className="header">Login</div>
+            <div className="content">
+                <div className="image">
+                    
+                </div>
+                <div className="form">
+                    <div className="from-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" placeholder="Username"/>
+                    </div>
+                    <div className="from-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" placeholder="email"/>
+                    </div>
+                    <div className="from-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" placeholder="Password"/>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <button type="button" className="btn">Register</button>
+            </div>        
+        </div>
+    }
+}
