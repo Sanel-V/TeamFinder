059d8f462bc5a0604f18f07e24d2b0230e4d6fac
Jonas Eszter <s7p5ga@inf.elte.hu>
2021-10-07 12:45:30 +0200

    Login/register interface done

--- frontend/teamfinder/src/components/login/register.jsx
+++ frontend/teamfinder/src/components/login/register.jsx
@@ -8,21 +8,21 @@ export class Register extends React.Component {
 
     render() {
         return <div className="base-container">
-            <div className="header">Login</div>
+            <div className="header">Register</div>
             <div className="content">
                 <div className="image">
                     
                 </div>
                 <div className="form">
-                    <div className="from-group">
+                    <div className="form-group">
                         <label htmlFor="username">Username</label>
                         <input type="text" name="username" placeholder="Username"/>
                     </div>
-                    <div className="from-group">
+                    <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" placeholder="email"/>
+                        <input type="email" name="email" placeholder="Email"/>
                     </div>
-                    <div className="from-group">
+                    <div className="form-group">
                         <label htmlFor="password">Password</label>
                         <input type="password" name="password" placeholder="Password"/>
                     </div>
