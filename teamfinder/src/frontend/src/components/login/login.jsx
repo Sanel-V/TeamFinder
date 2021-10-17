f25adbd22e53cbb378c367aa970bbdd431001a69
Jonas Eszter <s7p5ga@inf.elte.hu>
2021-10-17 21:14:16 +0200

    Revert "Link the login frontend to backend (CORS config missing)"
    This reverts commit cd52d6cef78dd36c211c916981b5554b621e83b6.

--- teamfinder/src/frontend/src/components/login/login.jsx
+++ teamfinder/src/frontend/src/components/login/login.jsx
@@ -1,42 +1,13 @@
 import React from "react";
 import loginImg from "../../login.svg";
-import axios from "axios";
 
 export class Login extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.baseUrl = "http://localhost:8080";
-
-        this.state = {
-            email: '',
-            password: ''
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.submit = this.submit.bind(this);
-
-    }
-
-    submit() {
-        const { email, password } = this.state;
-        axios.post(this.baseUrl + '/accounts/login', { email: email, password: password })
-        .then(response => {
-            console.log({response});
-        })
-        .catch(err => {
-            console.log({err});
-        });
-    }
-
-    handleChange(e) {
-        const {name, value} = e.target;
-        this.setState({[name]: value});
     }
 
     render() {
-        const {email, password} = this.state;
         return <div className="base-container">
             <div className="header">Login</div>
             <div className="content">
@@ -45,17 +16,17 @@ export class Login extends React.Component {
                 </div>
                 <div className="form">
                     <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="text" name="email" placeholder="Email" value={email} onChange={this.handleChange}/>
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" placeholder="Username"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange}/>
+                        <input type="password" name="password" placeholder="Password"/>
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="button" onClick={this.submit} className="btn">Login</button>
+                <button type="button" className="btn">Login</button>
             </div>        
         </div>
     }
--- teamfinder/src/frontend/src/components/login/register.jsx
+++ teamfinder/src/frontend/src/components/login/register.jsx
@@ -4,29 +4,9 @@ export class Register extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            username: '',
-            email: '',
-            password: ''
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.submit = this.submit.bind(this);
-    }
-
-    submit() {
-        const {username, email, password} = this.state;
-        console.log(this.state);
-    }
-
-    handleChange(e) {
-        const {name, value} = e.target;
-        this.setState({[name]: value});
     }
 
     render() {
-        const {username, email, password} = this.state;
         return <div className="base-container">
             <div className="header">Register</div>
             <div className="content">
@@ -36,20 +16,20 @@ export class Register extends React.Component {
                 <div className="form">
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange}/>
+                        <input type="text" name="username" placeholder="Username"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" placeholder="Email" value={email} onChange={this.handleChange}/>
+                        <input type="email" name="email" placeholder="Email"/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange}/>
+                        <input type="password" name="password" placeholder="Password"/>
                     </div>
                 </div>
             </div>
             <div className="footer">
-                <button type="button" onClick={this.submit} className="btn">Register</button>
+                <button type="button" className="btn">Register</button>
             </div>        
         </div>
     }
